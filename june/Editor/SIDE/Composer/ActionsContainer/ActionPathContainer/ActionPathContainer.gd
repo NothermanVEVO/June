dfd6aebc2403cbc0e4416781ab72a984326a96ce
Notherman <134525115+NothermanVEVO@users.noreply.github.com>
2026-03-09 18:14:05 -0300

    2026/03/09 - 001
    - Now you can ADD and ERASE ActionPathContainer.
    - You also can change their position, going UP or DOWN.
    - Now you can name each ActionPathContainer, any empty or valid int string will be converted to the index position.
    - Improved the way the ActionsContainer reopen, it'll save the last size before closing.

--- june/Editor/SIDE/Composer/ActionsContainer/ActionPathContainer/ActionPathContainer.gd
+++ june/Editor/SIDE/Composer/ActionsContainer/ActionPathContainer/ActionPathContainer.gd
@@ -1,1 +1,26 @@
 extends MarginContainer
+
+class_name ActionPathContainer
+
+@onready var _index_line_edit : LineEdit = $HBoxContainer/IndexLineEdit
+
+signal request_to_erase(action_path_container : ActionPathContainer)
+signal request_to_move_up(action_path_container : ActionPathContainer)
+signal request_to_move_down(action_path_container : ActionPathContainer)
+
+func update_index_text() -> void:
+	var text := _index_line_edit.text.strip_edges()
+	if text.is_empty() or text.is_valid_int():
+		_index_line_edit.text = str(get_index())
+
+func _on_erase_button_pressed() -> void:
+	request_to_erase.emit(self)
+
+func _on_move_up_button_pressed() -> void:
+	request_to_move_up.emit(self)
+
+func _on_move_down_button_pressed() -> void:
+	request_to_move_down.emit(self)
+
+func _on_index_line_edit_text_submitted(_new_text: String) -> void:
+	update_index_text()
--- june/Editor/SIDE/Composer/ActionsContainer/ActionsContainer.gd
+++ june/Editor/SIDE/Composer/ActionsContainer/ActionsContainer.gd
@@ -2,12 +2,19 @@ extends MarginContainer
 
 class_name ActionsContainer
 
-const MINIMUM_SIZE_Y : float = 60
-const Y_LIMIT : float = 200
+const _ACTION_PATH_CONTAINER_SCENE : PackedScene = preload("res://Editor/SIDE/Composer/ActionsContainer/ActionPathContainer/ActionPathContainer.tscn")
 
-var is_resizing : bool = false
-var mouse_position_when_down : Vector2
-var start_minimum_size_y : float
+## RESIZE VARS
+
+const _MINIMUM_SIZE_Y : float = 60
+const _Y_LIMIT : float = 200
+
+var _is_resizing : bool = false
+var _mouse_position_when_down : Vector2
+var _start_minimum_size_y : float
+var _last_size_y_before_close : float = 300
+
+## ONREADY THINGS
 
 @onready var _actions_container : ActionsContainer = $"."
 
@@ -24,19 +31,19 @@ func setup(pathway_editor : PathwayEditor, highest_grid_time : float) -> void:
 	_actions_vbox_container.setup(pathway_editor, highest_grid_time)
 
 func _process(delta: float) -> void:
-	if is_resizing:
-		custom_minimum_size.y = clampf(start_minimum_size_y + (mouse_position_when_down.y - get_global_mouse_position().y), 0, get_viewport_rect().size.y - Y_LIMIT)
+	if _is_resizing:
+		custom_minimum_size.y = clampf(_start_minimum_size_y + (_mouse_position_when_down.y - get_global_mouse_position().y), 0, get_viewport_rect().size.y - _Y_LIMIT)
 
 func _on_resize_button_button_up() -> void:
-	is_resizing = false
+	_is_resizing = false
 	set_process(false)
 
 func _on_resize_button_button_down() -> void:
 	if not _close_button.button_pressed:
 		return
-	is_resizing = true
-	mouse_position_when_down = get_global_mouse_position()
-	start_minimum_size_y = custom_minimum_size.y
+	_is_resizing = true
+	_mouse_position_when_down = get_global_mouse_position()
+	_start_minimum_size_y = custom_minimum_size.y
 	set_process(true)
 
 func _on_close_button_toggled(toggled_on: bool) -> void:
@@ -45,10 +52,47 @@ func _on_close_button_toggled(toggled_on: bool) -> void:
 		_title_rich_text_label.visible = true
 		_add_action_path_button.visible = true
 		_actions_container.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-		custom_minimum_size.y = 300
+		custom_minimum_size.y = _last_size_y_before_close
 	else:
 		_close_button.text = "Abrir editor de ações"
 		_title_rich_text_label.visible = false
 		_add_action_path_button.visible = false
 		_actions_container.size_flags_horizontal = Control.SIZE_SHRINK_END
+		_last_size_y_before_close = custom_minimum_size.y
 		custom_minimum_size.y = 0
+
+func _on_add_action_path_button_pressed() -> void:
+	var action_path_container : ActionPathContainer = _ACTION_PATH_CONTAINER_SCENE.instantiate()
+	_actions_vbox_container.add_child(action_path_container)
+	_actions_vbox_container.move_child(action_path_container, 0)
+	
+	_update_all_action_path_container_indexes()
+	
+	action_path_container.request_to_erase.connect(_requested_to_erase)
+	action_path_container.request_to_move_up.connect(_requested_to_move_up)
+	action_path_container.request_to_move_down.connect(_requested_to_move_down)
+
+func _requested_to_erase(action_path_container : ActionPathContainer) -> void:
+	_actions_vbox_container.remove_child(action_path_container)
+	action_path_container.queue_free()
+	
+	_update_all_action_path_container_indexes()
+
+func _requested_to_move_up(action_path_container : ActionPathContainer) -> void:
+	if _actions_vbox_container.get_child_count() <= 1:
+		return
+	
+	_actions_vbox_container.move_child(action_path_container, action_path_container.get_index() - 1)
+	_update_all_action_path_container_indexes()
+
+func _requested_to_move_down(action_path_container : ActionPathContainer) -> void:
+	if _actions_vbox_container.get_child_count() <= 1:
+		return
+	
+	_actions_vbox_container.move_child(action_path_container, action_path_container.get_index() + 1)
+	_update_all_action_path_container_indexes()
+
+func _update_all_action_path_container_indexes() -> void:
+	for child in _actions_vbox_container.get_children():
+		if child is ActionPathContainer:
+			child.update_index_text()
